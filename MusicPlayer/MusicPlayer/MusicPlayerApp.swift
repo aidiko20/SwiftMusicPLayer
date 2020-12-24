f19769125f51ce9f27f0b8c8481243ee0c9ecda3
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-24 00:02:53 -0500

    added starter pack

---
+++ MusicPlayer/MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Aida Yrysbekova on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
