c7e20ad819aa4ec6dca5a914cf8521772b32723c
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-24 01:04:38 -0500

    added AlbumCard and Album files

--- MusicPlayer/MusicPlayer/ContentView.swift
+++ MusicPlayer/MusicPlayer/ContentView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AlbumListView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("My Albums")
             .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AlbumListView()
     }
 }
